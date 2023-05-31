4ae7acbb4cac31c76f551fea9a6c0eef273991a6
Alcides Zelaya <alcideszelaya@hotmail.com>
2023-05-31 17:03:11 +0200

    [HEAP-43372] add ios example (#4)
    * add iOS binding bridge
    
    * [HEAP-43372] - use HeapSwiftCore-Dynamic.xcframework
    
    * udate xcframework
    
    * add HeapSwiftCoreInterfaces.xcframework
    
    * [HEAP-43372] add ios example
    
    * add user id and session id label

---
+++ iOS-Example/ViewController.cs
@@ -0,0 +1,104 @@
+﻿using Foundation;
+using System;
+using UIKit;
+using HeapSwiftCoreXamarin;
+
+namespace iOS_Example
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            updateLabel();
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        void updateLabel()
+        {
+            label.Text = "UserId: " + Heap.SharedInstance.UserId + "\nSessionId: " + Heap.SharedInstance.SessionId;
+        }
+
+        partial void startRecordingTapped(UIButton sender)
+        {
+            Console.WriteLine("startRecordingTapped...");
+            Heap.SharedInstance.StartRecording("833626842");
+            updateLabel();
+        }
+
+        partial void stopRecordingTapped(UIButton sender)
+        {
+            Console.WriteLine("stopRecordingTapped...");
+            Heap.SharedInstance.StopRecording();
+            updateLabel();
+        }
+
+        partial void identifyTapped(UIButton sender)
+        {
+            Console.WriteLine("identifyTapped");
+            Heap.SharedInstance.Identify("Xamarin user");
+            updateLabel();
+        }
+
+        partial void resetIdentityTapped(UIButton sender)
+        {
+            Console.WriteLine("resetIdentityTapped");
+            Heap.SharedInstance.ResetIdentity();
+            updateLabel();
+        }
+
+        partial void trackTapped(UIButton sender)
+        {
+            Console.WriteLine("trackTapped");
+            Heap.SharedInstance.Track("My event");
+            updateLabel();
+        }
+
+        partial void trackWithPropertiesTapped(UIButton sender)
+        {
+            Console.WriteLine("Tracking with properties");
+            var properties = new NSDictionary("key1-string", "my-value 1", "key2-number", 256);
+            Heap.SharedInstance.Track("My event with properties", properties);
+            updateLabel();
+        }
+
+        partial void addEventPropertiesTapped(UIButton sender)
+        {
+            Console.WriteLine("addEventPropertiesTapped");
+            var properties = new NSDictionary("my-event-property-string", "my-value 1", "my-event-property-number", 512);
+            Heap.SharedInstance.AddEventProperties(properties);
+            updateLabel();
+        }
+
+        partial void addUserProperties(UIButton sender)
+        {
+            Console.WriteLine("addUserProperties");
+            var properties = new NSDictionary("my-user-property-string", "my-value 1", "my-user-property-number", 1024);
+            Heap.SharedInstance.AddUserProperties(properties);
+            updateLabel();
+        }
+
+        partial void clearEventPropertiesTapped(NSObject sender)
+        {
+            Console.WriteLine("clearEventPropertiesTapped...");
+            Heap.SharedInstance.ClearEventProperties();
+            updateLabel();
+        }
+
+        partial void removeEventPropertyTapped(UIButton sender)
+        {
+            Console.WriteLine("removeEventPropertyTapped...");
+            Heap.SharedInstance.RemoveEventProperty("my-event-property-number");
+            updateLabel();
+        }
+    }
+}
---
+++ iOS-Example/ViewController.designer.cs
@@ -0,0 +1,56 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace iOS_Example
+{
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		UIKit.UILabel label { get; set; }
+
+		[Action ("addEventPropertiesTapped:")]
+		partial void addEventPropertiesTapped (UIKit.UIButton sender);
+
+		[Action ("addUserProperties:")]
+		partial void addUserProperties (UIKit.UIButton sender);
+
+		[Action ("clearEventPropertiesTapped:")]
+		partial void clearEventPropertiesTapped (Foundation.NSObject sender);
+
+		[Action ("identifyTapped:")]
+		partial void identifyTapped (UIKit.UIButton sender);
+
+		[Action ("removeEventPropertyTapped:")]
+		partial void removeEventPropertyTapped (UIKit.UIButton sender);
+
+		[Action ("resetIdentityTapped:")]
+		partial void resetIdentityTapped (UIKit.UIButton sender);
+
+		[Action ("startRecordingTapped:")]
+		partial void startRecordingTapped (UIKit.UIButton sender);
+
+		[Action ("stopRecordingTapped:")]
+		partial void stopRecordingTapped (UIKit.UIButton sender);
+
+		[Action ("trackTapped:")]
+		partial void trackTapped (UIKit.UIButton sender);
+
+		[Action ("trackWithPropertiesTapped:")]
+		partial void trackWithPropertiesTapped (UIKit.UIButton sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (label != null) {
+				label.Dispose ();
+				label = null;
+			}
+		}
+	}
+}
